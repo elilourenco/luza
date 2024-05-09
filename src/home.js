db7c19b41689429c707620fcc43caccfd9c91846
elilourenco <lourencomonica318@gmail.com>
2024-05-09 16:11:28 +0100

    modify the buttons

--- src/home.js
+++ src/home.js
@@ -1,6 +1,12 @@
 const startslide = ()=>{
-    const slidebutton= document.querySelector("")
+    const slidebutton= document.querySelectorAll(".before-slide .after.slide");
+     slidebutton.forEach(button => {
 
+        button.addEventListener("click",()=>{
+            console.log(button)
+        })
+        
+     });
 
 }
 window.addEventListener("load", startslide);
